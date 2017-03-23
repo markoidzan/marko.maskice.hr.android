142933242240c9a1d70c7efc22ce2868d1571ee1
markoidzan <marko.idzan@gmail.com>
2017-03-23 13:27:34 +0100

    PhoneGap Test
    Public Repo Triout

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'maskice-hr',
+		wp_ws_url : 'http://maskice.hr/wp-appkit-api/maskice-hr',
+		wp_url : 'http://maskice.hr',
+		theme : 'q',
+		version : '1.0.0',
+		app_title : 'maskice.hr',
+		app_platform : 'android',
+		gmt_offset : 1,
+		debug_mode : 'on',
+		auth_key : 'rasglrkzjxsr4isfxttkcfgujj0gqwcbceo8xlggaifzpnq7zureg5ln7spnv7gm',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
